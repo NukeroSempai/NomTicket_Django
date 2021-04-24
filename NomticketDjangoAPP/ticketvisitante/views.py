196c1e9238bcbe036503ea0d3868c2b608bbbdb8
Ignacio <ignacio.prieto1985@gmail.com>
2021-04-24 19:53:47 -0400

    Frontend Django v01

---
+++ NomticketDjangoAPP/ticketvisitante/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def login_visitante(request):
+    return render(request, 'ticketvisitante/login_visitante.html')
+
+def visitante(request):
+    return render(request, 'ticketvisitante/visitante.html')

d793ffe8fa30868ffff744c90d3dcce674237a7a
Ignacio <ignacio.prieto1985@gmail.com>
2021-04-26 20:41:12 -0400

    Django 26/4

--- NomticketDjangoAPP/ticketempleado/views.py
+++ NomticketDjangoAPP/ticketempleado/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from CORE.models import PRODUCTO
 
 # Create your views here.
 def tickets_emitidos(request):
@@ -8,7 +9,8 @@ def login_empleado(request):
     return render(request, 'ticketempleado/login_empleado.html')
 
 def empleado(request):
-    return render(request, 'ticketempleado/empleado.html')
+    productos = PRODUCTO.objects.all().order_by('codigo_producto')
+    return render(request, 'ticketempleado/empleado.html', {'productos' : productos })
 
 def ticket_empleado(request):
     return render(request, 'ticketempleado/ticket_empleado.html')
